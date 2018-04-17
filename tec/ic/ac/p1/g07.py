4d1018e5d54560a496027226dff35ee4d854a565
Mauro <mauromm1996@gmail.com>
2018-04-16 21:08:56 -0600

    Agrega el paquete@

---
+++ tec/ic/ac/p1/g07.py
@@ -0,0 +1,11 @@
+import argparse
+
+parser = argparse.ArgumentParser(description='This program allows to train a model based on Costa Rica\'s elections.')
+parser.add_argument('integers', metavar='N', type=int, nargs='+',
+                    help='an integer for the accumulator')
+parser.add_argument('--sum', dest='accumulate', action='store_const',
+                    const=sum, default=max,
+                    help='sum the integers (default: find the max)')
+
+args = parser.parse_args()
+print(args.accumulate(args.integers))
