000084679913a59aa8c21650d5cb23e56184b89f
valebm <valebm18@gmail.com>
2018-05-04 10:01:09 -0600

    Changes format in K_Nearest_Neighbors.py

--- tec/ic/ia/p1/models/K_Nearest_Neighbors.py
+++ tec/ic/ia/p1/models/K_Nearest_Neighbors.py
@@ -13,21 +13,29 @@ class KNearestNeighbors(Model):
     # Recursive function that creates the kd tree using a dictionary.
     # Input: The samples being distributed and the dimension of the tree.
     # Output: A dictionary containing the built tree.
-    def create_kdtree(self, samples, dimension=0):
+    def create_kdtree(self, samples, level=0):
+        # The samples' informaction is separated.
         samples_data = samples[0]
         samples_vote = samples[1]
         n = len(samples_data)
+        # If there are no samples
         if n <= 0:
             return None
-        breaking_point = dimension % len(samples_data[0])
+        # The splitting attribute to make the two branches is chosen
+        breaking_point = level % len(samples_data[0])
+        # The samples are ordered so they can be separated for the two branches
         ordered_samples, ordered_votes = (list(t) for t in zip(
             *sorted(zip(samples_data, samples_vote), key=lambda pair: pair[0][breaking_point])))
         half = round(n / 2)
+        # A tree node is built containing its sample, its votes, and its left
+        # and right branches (which are built recursively)
         return {
             'sample': ordered_samples[half],
             'vote': ordered_votes[half],
-            'left_son': self.create_kdtree([ordered_samples[:half], ordered_votes[:half]], dimension + 1),
-            'right_son': self.create_kdtree([ordered_samples[half + 1:], ordered_votes[half + 1:]], dimension + 1)
+            'left_son': self.create_kdtree([ordered_samples[:half], 
+                ordered_votes[:half]], level + 1),
+            'right_son': self.create_kdtree([ordered_samples[half + 1:],
+                ordered_votes[half + 1:]], level + 1)
         }
 
     # Function that calculates the Manhattan Distance (L1) between two points.
@@ -36,48 +44,66 @@ class KNearestNeighbors(Model):
     def calculate_manhattan_distance(self, p1, p2):
         return sum(abs(x - y) for x, y in zip(p1, p2))
 
-    # Recursive function that looks for the k closest points to the one being classified.
-    # Input: The root of the kd tree, the point being classified and the current level of the tree.
+    # Recursive function that looks for the k closest points to the one being
+    # classified.
+    # Input: The root of the kd tree, the point being classified and the current
+    # level of the tree.
     # Output: A list containing the list of votes of the closest points, and a
     # list of their distances to the point.
-    def kdtree_closest_point(self, root, point, depth=0):
+    def kdtree_closest_point(self, root, point, level=0):
         knn_best = []
         knn_distances = []
+        # If root's parent is a leaf
         if root is None:
             return None
-        axis = depth % len(point)
+        # The attribute is chosen to classify the point into a branch (the same
+        # attribute chosen for the tree building)
+        axis = level % len(point)
         next_branch = None
         opposite_branch = None
+        # The next_branch corresponds to the branch to which the point being 
+        # classified belongs.
+        # The opposite_branch is the other branch.
         if point[axis] < root['sample'][axis]:
             next_branch = root['left_son']
             opposite_branch = root['right_son']
         else:
             next_branch = root['right_son']
             opposite_branch = root['left_son']
+        # The distanche between the root and the classified point is
+        # calculated.
         root_distance = self.calculate_manhattan_distance(
             point, root['sample'])
-        best_left = self.kdtree_closest_point(next_branch, point, depth + 1)
-        if best_left is None:
-            best_left = [[], []]
-        knn_best = best_left[0] + [root['vote']]
-        knn_distances = best_left[1] + [root_distance]
+        # The closest k points in the next_branch are calculated recursively.
+        best_next = self.kdtree_closest_point(next_branch, point, level + 1)
+        if best_next is None:
+            best_next = [[], []]
+        knn_best = best_next[0] + [root['vote']]
+        knn_distances = best_next[1] + [root_distance]
+        # The closest neighbors are ordered by distance.
         knn_distances, knn_best = (list(t) for t in zip(
             *sorted(zip(knn_distances, knn_best), key=lambda pair: pair[0])))
-        best_right = []
+        # If the distance from the point to the root in the axis dimension is
+        # lower than the greatest k best distance, the closest k points in the
+        # opposite_branch are calculated recursively.
+        best_opposite = []
         if abs(point[axis] - root['sample'][axis]
                ) < knn_distances[:self.k][-1]:
-            best_right = self.kdtree_closest_point(
-                opposite_branch, point, depth + 1)
-            if best_right is None:
-                best_right = [[], []]
+            best_opposite = self.kdtree_closest_point(
+                opposite_branch, point, level + 1)
+            if best_opposite is None:
+                best_opposite = [[], []]
 
-            knn_best += best_right[0]
-            knn_distances += best_right[1]
+            knn_best += best_opposite[0]
+            knn_distances += best_opposite[1]
+            # The closest neighbors are ordered by distance.
             knn_distances, knn_best = (list(t) for t in zip(
                 *sorted(zip(knn_distances, knn_best), key=lambda pair: pair[0])))
+        # Only the k closest neighbors are returned.
         return [knn_best[:self.k], knn_distances[:self.k]]
 
-    # Function that its called to execute the model. It creates the tree, and tests it using the training and testing set.
+    # Function that its called to execute the model. It creates the tree, and tests 
+    # it using the training and testing set.
     # Input: -
     # Output: A list containing the predicted votes for the training and
     # testing set.
@@ -96,32 +122,44 @@ class KNearestNeighbors(Model):
         p_results = 0
         n_results = 0
         for i in range(0, len(self.samples_train[0])):
+            # Tests point agains kd tree and takes the predited votes
             knn_result = self.kdtree_closest_point(
                 tree, self.samples_train[0][i])
             votes = knn_result[0]
             data = Counter(votes)
             sample_result = self.samples_train[1][i]
             pred_result = max(votes, key=data.get)
+            # Compares the predited votes against real ones
             if sample_result == pred_result:
                 p_results += 1
             else:
                 n_results += 1
             pred_votes.append(pred_result)
-        print("Error con train set: ", n_results / len(self.samples_train[0]))
+        print("Training set")
+        print("Aciertos: ", p_results, " De: ", self.samples_train[0], " Accuracy: ",
+              p_results / len(self.samples_train[0]), "%")
+        print("n= ", n_results)
+        print("p= ", p_results)
         # Measures error with testing data
-        p_results = 0
-        n_results = 0
+        p_results=0
+        n_results=0
         for i in range(0, len(self.samples_test[0])):
-            knn_result = self.kdtree_closest_point(
+            # Tests point agains kd tree and takes the predited votes
+            knn_result=self.kdtree_closest_point(
                 tree, self.samples_test[0][i])
-            votes = knn_result[0]
-            data = Counter(votes)
-            sample_result = self.samples_test[1][i]
-            pred_result = max(votes, key=data.get)
+            votes=knn_result[0]
+            data=Counter(votes)
+            sample_result=self.samples_test[1][i]
+            pred_result=max(votes, key = data.get)
+            # Compares the predited votes against real ones
             if sample_result == pred_result:
                 p_results += 1
             else:
                 n_results += 1
             pred_votes.append(pred_result)
-        print("Error con test set: ", n_results / len(self.samples_test[0]))
+        print("Testing set")
+        print("Aciertos: ", p_results, " De: ", self.samples_test[0], " Accuracy: ",
+              p_results / len(self.samples_test[0]), "%")
+        print("n= ", n_results)
+        print("p= ", p_results)
         return pred_votes
